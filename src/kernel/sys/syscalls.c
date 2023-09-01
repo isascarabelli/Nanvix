ec64e2cd9871199ef61c7919c9dd5f26c8c67e75
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2023-09-01 13:31:52 -0300

    Update syscalls.c

--- src/kernel/sys/syscalls.c
+++ src/kernel/sys/syscalls.c
@@ -73,8 +73,8 @@ PUBLIC void (*syscalls_table[NR_SYSCALLS])(void)  = {
 	(void (*)(void))&sys_shutdown,
 	(void (*)(void))&sys_ps,
 	(void (*)(void))&sys_gticks,
-	(void (*)(void))&sys_get_process_info,
-    NULL,
-    NULL,
-    NULL
+	 NULL,
+         NULL,
+         NULL,
+        (void (*)(void))&sys_get_process_info,
 };
