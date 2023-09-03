cb0d697c54b9b39e70288bfd424339f2aa9d063d
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2023-09-03 15:54:49 -0300

    Update get_proc_info.c

--- src/kernel/pm/get_proc_info.c
+++ src/kernel/pm/get_proc_info.c
@@ -1,64 +1,14 @@
 /*
  Implementação da chamada de kernel sys_get_process_info() 
  que invoca a função de kernel do_get_process_info(), 
- implementada em src/kernel/pm/pm.c e mostra na tela para o usuário
-os dados daquele processo contido em process_buf.
+ implementada em src/kernel/pm/pm.c. Ela é invocada em 
+ getp.c por NR_get_process, para realizar a passagem
+ do espaço de usuário para o espaço de kernel.
  */
 
 #include <nanvix/klib.h>
-#include <nanvix/pm.h>
-
-//Formatação
-void prepareValue(int value, char* s, int padding)
-{
-	int i,len,size;
-
-	itoa (value, s, 10);
-	len = padding - kstrlen(s);
-
-	size = kstrlen(s);
-	for(i=size; i<len+size; i++)
-		*(s+i) = ' ';
-
-	*(s+i) = '\0';
-}
+#include <nanvix/pm.h> 
 
 PUBLIC void  sys_get_process_info( pid_t pid, struct process_buf *buf ){
 	do_get_process_info( pid, buf );
-
-	//Parâmetros abaixo usados apenas para formatação na hora de imprimir para o usuário
-	char p[26];
-	char priority[26];
-	char utime   [26];
-	char ktime   [26];
-
-	const char *states[7];
-	states[0] = "DEAD";
-	states[1] = "ZOMBIE";
-	states[2] = "RUNNING";
-	states[3] = "READY";
-	states[4] = "WAITING";
-	states[5] = "SLEEPING";
-	states[6] = "STOPPED";
-
-	kprintf("------------------------------- Process Status"
-			" -------------------------------\n"
-		    "PID    PRIORITY "
-		    "   UTIME   KTIME     STATUS");
-
-		/* Pid */
-		prepareValue(buf->pid, p, 6);
-
-		/* Priority */
-		prepareValue(buf->priority, priority, 11);
-
-		/* Utime */
-		prepareValue(buf->utime, utime, 8);
-
-		/* Ktime */
-		prepareValue(buf->ktime, ktime, 10);
-
-		kprintf("%s%s%s%s%s%s%s%s", pid,
-			priority, utime, ktime, states[(int)buf->state] );
 }
-
