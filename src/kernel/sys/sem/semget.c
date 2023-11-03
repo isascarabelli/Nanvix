1893784a0e66c65b678011a2c98739fc7387729f
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-11-03 09:04:08 -0300

    Create semget.c

---
+++ src/kernel/sys/sem/semget.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ *
+ * This file is part of Nanvix.
+ *
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+* @brief Creates a semaphore or returns the key.
+*/
+
+#include <nanvix/const.h>
+#include <nanvix/hal.h>
+#include <nanvix/klib.h>
+#include <nanvix/pm.h>
+#include <sys/sem.h>
+
+PUBLIC void sys_semget(unsigned key){
+
+    struct semaphore *sem;
+
+    for (sem = 0; sem < SEM_MAX; sem++){
+        if (sem->id == key){
+            return key;
+        } else {
+            sem->id = key;
+        }
+    }
+
+}
