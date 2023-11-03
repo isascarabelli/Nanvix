4e119ac8dd3c6a89e3c5216ae775ae18ad4a2f39
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-11-03 09:02:15 -0300

    Update sem.h

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -24,14 +24,24 @@
 	 * @brief Comand values for semaphores.
 	 */
 	/**@{*/
-	#define GETVAL   0 /**< Returns the value of a semaphore. */
-	#define SETVAL   1 /**< Sets the value of a semaphore.    */
-	#define IPC_RMID 3 /**< Destroys a semaphore.            */
+	#define GETVAL   0   /**< Returns the value of a semaphore.  */
+	#define SETVAL   1   /**< Sets the value of a semaphore.     */
+	#define IPC_RMID 3   /**< Destroys a semaphore.              */
+	#define SEM_MAX  10  /**< Set the max value for a semaphore. */
 	/**@}*/
 
 	/* Forward definitions. */
 	extern int semget(unsigned);
 	extern int semctl(int, int, int);
 	extern int semop(int, int);
+
+	/* Struct that keeps semaphore information */
+	struct semaphore
+	{
+		int id;
+		unsigned val;
+		unsigned curr_val;
+
+	};
 
 #endif /* SEM_H_ */
