9c864a83361741621100d852d4fa549140577203
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2023-11-05 18:34:11 -0300

    Update syscalls.c

--- src/kernel/sys/syscalls.c
+++ src/kernel/sys/syscalls.c
@@ -74,7 +74,7 @@ PUBLIC void (*syscalls_table[NR_SYSCALLS])(void)  = {
 	(void (*)(void))&sys_ps,
 	(void (*)(void))&sys_gticks,
 	(void (*)(void))&sys_get_process_info,
-	 NULL,
-         NULL,
-         NULL,
+	(void (*)(void))&sys_semget,
+        (void (*)(void))&sys_semctl,
+        (void (*)(void))&sys_semop,
 };
