c01c19a5b64c10c705e476c1bc99e4c7a2a9fb32
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-11-06 10:50:23 -0300

    Update semop.c

--- src/kernel/sys/sem/semop.c
+++ src/kernel/sys/sem/semop.c
@@ -17,12 +17,17 @@
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+* @brief Down and Up operations on semaphore.
+*/
+
 #include <nanvix/const.h>
 #include <nanvix/hal.h>
 #include <nanvix/klib.h>
 #include <nanvix/pm.h>
 #include <sys/sem.h>
 
+//Current value decrement or sleeping process
 PUBLIC void down_sem(struct semaphore *sem, int semid){
 
     if (sem->curr_val > 0)
@@ -32,6 +37,7 @@ PUBLIC void down_sem(struct semaphore *sem, int semid){
 
 }
 
+//Current value increment and sleeping process
 PUBLIC void up_sem(struct semaphore *sem, int semid){
 
     if (sem->curr_val == 0){
@@ -42,10 +48,11 @@ PUBLIC void up_sem(struct semaphore *sem, int semid){
 
 }
 
-PUBLIC void sys_semop(int semid, int op){
+PUBLIC int sys_semop(int semid, int op){
 
     struct semaphore *sem;
 
+    //Searching for semid
     for (sem = FIRST_SEM; sem < LAST_SEM; sem++){
         if (sem->id == semid){
             if (op < 0) {
@@ -55,5 +62,7 @@ PUBLIC void sys_semop(int semid, int op){
             }
         }
     }
+
+    return 0;
 
 }
