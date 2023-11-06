1934def494384646b16940190cd1d0da82dff089
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-11-06 10:51:31 -0300

    Update sem.h

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -16,14 +16,19 @@
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef SEM_H_
 #define SEM_H_
+
+	#include <nanvix/const.h>
 
 	/**
 	 * @brief Comand values for semaphores.
 	 */
 	/**@{*/
+	#define GETVAL   0   /**< Returns the value of a semaphore.  */
+	#define SETVAL   1   /**< Sets the value of a semaphore.     */
+	#define IPC_RMID 3   /**< Destroys a semaphore.              */
+	#define SEM_MAX  10  /**< Set the max value for a semaphore. */
 	#define GETVAL   0   				/**< Returns the value of a semaphore.  */
 	#define SETVAL   1   				/**< Sets the value of a semaphore.     */
 	#define IPC_RMID 3   				/**< Destroys a semaphore.              */
@@ -33,13 +38,16 @@
 	/**@}*/
 
 	/* Forward definitions. */
-	extern int semget(unsigned);
-	extern int semctl(int, int, int);
-	extern int semop(int, int);
-
+	EXTERN int semget(unsigned);
+	EXTERN int semctl(int, int, int);
+	EXTERN int semop(int, int);
+	
 	/* Struct that keeps semaphore information */
 	struct semaphore
 	{
+		int id;
+		unsigned val;
+		unsigned curr_val;
 		int id;				/**< Semaphore ID. */
 		unsigned val;		/**< Top value of semaphore. */
 		unsigned curr_val;	/**< Current value of semaphore. */
@@ -47,6 +55,6 @@
 	};
 
 	/* Forward definitions. */
-	extern struct semaphore semtab[SEM_MAX];
+	EXTERN struct semaphore semtab[SEM_MAX];
 
 #endif /* SEM_H_ */
