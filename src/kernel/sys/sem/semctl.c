5863135052c88f3abf308fa433d30313e4540f11
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-11-19 14:42:37 -0300

    Update semctl.c

--- src/kernel/sys/sem/semctl.c
+++ src/kernel/sys/sem/semctl.c
@@ -40,13 +40,10 @@ PUBLIC int sys_semctl(int semid, int cmd, int val){
                 semtab[semid].val = val;     //Set max value of semaphore.
             if (cmd == IPC_RMID && sem->flag == UNLOCKED){
                 semtab[semid].id = -1;       //Delete semaphore id.
-                semtab[semid].flag = UNLOCKED;
-                semtab[semid].val = 1;
-                semtab[semid].curr_val = 1;
             } else {
                 wakeup(curr_proc->chain);
-                semtab[semid].id = -1;
                 semtab[semid].flag = UNLOCKED;
+                semtab[semid].id = -1;
                 semtab[semid].val = 1;
                 semtab[semid].curr_val = 1;
             }
