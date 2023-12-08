051e4ef42c5235e615a8ca37e7c710be06bcd247
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2023-12-08 18:26:26 -0300

    Update paging.h
    adding referenced bit

--- include/i386/paging.h
+++ include/i386/paging.h
@@ -20,67 +20,67 @@
 #ifndef I386_PAGING_H_
 #define I386_PAGING_H_
 
-	/* Shifts and masks. */
-	#define PAGE_SHIFT  12                  /* Page shift.                 */
-	#define PGTAB_SHIFT 22                  /* Page table shift.           */
-	#define PAGE_MASK   (~(PAGE_SIZE - 1))  /* Page mask.                  */
-	#define PGTAB_MASK  (~(PGTAB_SIZE - 1)) /* Page table mask.            */
+/* Shifts and masks. */
+#define PAGE_SHIFT 12                  /* Page shift.                 */
+#define PGTAB_SHIFT 22                 /* Page table shift.           */
+#define PAGE_MASK (~(PAGE_SIZE - 1))   /* Page mask.                  */
+#define PGTAB_MASK (~(PGTAB_SIZE - 1)) /* Page table mask.            */
 
-	/* Sizes. */
-	#define PAGE_SIZE  (1 << PAGE_SHIFT) /* Page size.                 */
-	#define PGTAB_SIZE (1 << PGTAB_SHIFT) /* Page table size.           */
-	#define PTE_SIZE   4                 /* Page table entry size.     */
-	#define PDE_SIZE   4                 /* Page directory entry size. */
+/* Sizes. */
+#define PAGE_SIZE (1 << PAGE_SHIFT)   /* Page size.                 */
+#define PGTAB_SIZE (1 << PGTAB_SHIFT) /* Page table size.           */
+#define PTE_SIZE 4                    /* Page table entry size.     */
+#define PDE_SIZE 4                    /* Page directory entry size. */
 
 #ifndef _ASM_FILE_
 
-	/*
-	 * Page directory entry.
-	 */
-	struct pde
-	{
-		unsigned present  :  1; /* Present in memory? */
-		unsigned writable :  1; /* Writable page?     */
-		unsigned user     :  1; /* User page?         */
-		unsigned          :  2; /* Reserved.          */
-		unsigned accessed :  1; /* Accessed?          */
-		unsigned dirty    :  1; /* Dirty?             */
-		unsigned          :  2; /* Reserved.          */
-		unsigned          :  3; /* Unused.            */
-		unsigned frame    : 20; /* Frame number.      */
-	};
+/*
+ * Page directory entry.
+ */
+struct pde
+{
+    unsigned present : 1;  /* Present in memory? */
+    unsigned writable : 1; /* Writable page?     */
+    unsigned user : 1;     /* User page?         */
+    unsigned : 2;          /* Reserved.          */
+    unsigned accessed : 1; /* Accessed?          */
+    unsigned dirty : 1;    /* Dirty?             */
+    unsigned : 2;          /* Reserved.          */
+    unsigned : 3;          /* Unused.            */
+    unsigned frame : 20;   /* Frame number.      */
+};
 
-	/*
-	 * Page table entry.
-	 */
-	struct pte
-	{
-		unsigned present  :  1; /* Present in memory? */
-		unsigned writable :  1; /* Writable page?     */
-		unsigned user     :  1; /* User page?         */
-		unsigned          :  2; /* Reserved.          */
-		unsigned accessed :  1; /* Accessed?          */
-		unsigned dirty    :  1; /* Dirty?             */
-		unsigned          :  2; /* Reserved.          */
-		unsigned cow      :  1; /* Copy on write?     */
-		unsigned zero     :  1; /* Demand zero?       */
-		unsigned fill     :  1; /* Demand fill?       */
-		unsigned frame    : 20; /* Frame number.      */
-	};
+/*
+ * Page table entry.
+ */
+struct pte
+{
+    unsigned present : 1;    /* Present in memory?             */
+    unsigned writable : 1;   /* Writable page?                 */
+    unsigned user : 1;       /* User page?                     */
+    unsigned : 2;            /* Reserved.                      */
+    unsigned accessed : 1;   /* Accessed?                      */
+    unsigned dirty : 1;      /* Dirty? Modified Bit            */
+    unsigned : 2;            /* Reserved.                      */
+    unsigned cow : 1;        /* Copy on write?                 */
+    unsigned zero : 1;       /* Demand zero?                   */
+    unsigned fill : 1;       /* Demand fill?                   */
+    unsigned frame : 20;     /* Frame number.                  */
+    unsigned referenced : 1; /* Referenced in 8 acess?         */
+};
 
-	/*
-	 * DESCRIPTION;
-	 *   The PG() macro returns the page number where a given virtual address.
-	 */
-	#define PG(a) (((unsigned)(a) & (PGTAB_MASK^PAGE_MASK)) >> PAGE_SHIFT)
+/*
+ * DESCRIPTION;
+ *   The PG() macro returns the page number where a given virtual address.
+ */
+#define PG(a) (((unsigned)(a) & (PGTAB_MASK ^ PAGE_MASK)) >> PAGE_SHIFT)
 
-	/*
-	 * DESCRIPTION:
-	 *   The PGTAB() macro returns the page table number of a given virtual
-	 */
-	#define PGTAB(a) ((unsigned)(a) >> PGTAB_SHIFT)
+/*
+ * DESCRIPTION:
+ *   The PGTAB() macro returns the page table number of a given virtual
+ */
+#define PGTAB(a) ((unsigned)(a) >> PGTAB_SHIFT)
 
 #endif /* _ASM_FILE_ */
 
 #endif /* I386_PAGING_H_ */
-
